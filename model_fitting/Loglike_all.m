86e612af6ab9f70082ed2cc6c5616df2d21d24ee
Andra Mihali <alm652@nyu.edu>
2020-06-27 23:01:44 -0400

    Update Loglike_all.m

--- model_fitting/Loglike_all.m
+++ model_fitting/Loglike_all.m
@@ -1,10 +1,10 @@
 function LL = Loglike_all(pars, data,mi, type)
 
-%type=1 -> Det_pred. output=[ptp_p, ptp_a]- 8 val
-%type=2 -> Loc_pred. output=[pc]- 4 val
-%type=3 -> Det_Loc_pred. output=[ptp, ptp_a, pc]- 12 val
+%type=1 -> Det
+%type=2 -> Loc
+%type=3 -> Det & Loc joint
 
-Nsamp = 2000;%3000;%2000;
+Nsamp = 1200;%2000;%3000;%2000;
 
 
 if type==1
@@ -18,7 +18,7 @@ end
 
 
 J1bar = exp(pars(1:4)); %mean of the gamma distribution of J1
-tau   = exp(pars(5));
+tau   = exp(pars(5)); % scale of the gamma distribution
 if ismember(type,[1 3])
     pp    = pars(6);
     nvars = 6;
@@ -26,14 +26,16 @@ elseif type == 2
     nvars = 5;
 end
 
-if mi == 2 % decision noise
+% model 2 with decision noise; for type 3 - 2 diff decision noise values
+% for det and loc
+if mi == 2 & type <3 
     alpha = exp(pars(nvars+1));
     nvars = nvars+1;
+elseif mi == 2 & type == 3
+    alpha1 = exp(pars(nvars+1));
+    alpha2 = exp(pars(nvars+2));
+    nvars = nvars+2;   
 end
-
-
-lapse_yes = 0;
-
 
 
 % Unpack data
@@ -44,7 +46,7 @@ if type==1 | type==3
     Ntrials_d = length(data_d.N);
     sdata_d   = data_d.stims;
     Cdata_d   = data_d.target_pres;
-    Ldata_d   = data_d.target_loc; %irrelevant here
+    Ldata_d   = data_d.target_loc; 
     Resp_d    = data_d.response;
     Nvec_d    = unique(Ndata_d);
     target_d  = data_d.target_val;
@@ -86,15 +88,13 @@ if (type==1) | (type==3)
         dec_var_global = squeeze(log(mean(exp(dec_var),2)) + log(pp/(1-pp)));
         if mi == 1
             ptp(idx) = mean(dec_var_global>0,2);
-        elseif mi == 2  %decision noise
+        elseif mi == 2 & type == 2  
             ptp(idx) = mean(1./(1+ exp(-alpha * dec_var_global)),2);
+        elseif mi == 2 & type == 3
+            ptp(idx) = mean(1./(1+ exp(-alpha1 * dec_var_global)),2);
         end
         
-        if lapse_yes
-            %lapse_val_d=lapse_d;
-            lapse_val_d = lapse_d(Nind);
-            ptp(idx) = lapse_val_d*1/2+(1-lapse_val_d)*ptp(idx);
-        end
+        
     end
     
     ptp(ptp==0) = 1/Nsamp;
@@ -108,11 +108,16 @@ if (type==1) | (type==3)
 end
 
 if (type==2) | (type==3)
-    % Computing probability of responding "corect" on each trial
+    % Computing probability of responding "Lhat" on each trial
     pc = NaN(Ntrials_l,1);
-    Ldata_le=NaN(Ntrials_l,1);
-    matr=cumsum(isnan(sdata_l),2);
-    Ldata_le=Ldata_l-matr(sub2ind(size(matr),[1:1:Ntrials_l]',Ldata_l));
+    Ldata_le = NaN(Ntrials_l,1);
+    matr = cumsum(isnan(sdata_l),2);
+    Ldata_le = Ldata_l-matr(sub2ind(size(matr),[1:1:Ntrials_l]',Ldata_l));
+    Resp_le =Resp_l - matr(sub2ind(size(matr),[1:1:Ntrials_l]',Resp_l));
+    
+    ddr = NaN(Ntrials_l, Nsamp);
+    prob_l = NaN(Ntrials_l,6);
+    plhat = NaN(Ntrials_l,1);
     
     for Nind = 1:length(Nvec_l)
         N         = Nvec_l(Nind);
@@ -126,33 +131,57 @@ if (type==2) | (type==3)
         sdata_le = sdata_le(~isnan(sdata_le));
         sdata_le = reshape(sdata_le,N,length(idx))';
         
-        stims     = repmat(sdata_le(:,1:N),[1,1, Nsamp]); % Read in data -keep all 6 vals, also nans'. and replicate
+        stims     = repmat(sdata_le(:,1:N),[1,1, Nsamp]); % Read in data -keep all 6 vals, also nan and replicate
         x         = qrandvm(stims,kappa,[length(idx) N Nsamp]); %circ_vmrnd(stims,kappa);
         dec_var = -log(besseli(0,kappa,1))-kappa+kappa.*cos(bsxfun(@minus,x,target_l(idx)));
-        if (mi == 1) | (mi == 2 & type == 3) % do not add decision noise to localization in the joint type
-            [mx,dd]   = max(dec_var,[],2);
-            pc(idx)   = mean(bsxfun(@eq,dd, Ldata_le(idx)),3);
-        elseif (mi == 2) & (type == 2)
-            post_L_given_x = bsxfun(@rdivide, exp(dec_var), sum(exp(dec_var),2));
-            post_L_given_x_noisy = post_L_given_x.^alpha;
-            post_L_given_x_noisy = bsxfun(@rdivide, post_L_given_x_noisy, sum(post_L_given_x_noisy,2)); %renormalize
-            post_L_given_x_noisy_avg = squeeze(nanmean(post_L_given_x_noisy,3));
-            pc(idx) = post_L_given_x_noisy_avg(sub2ind(size(post_L_given_x_noisy_avg),[1:1:length(idx)]',Ldata_le(idx)));
+        post_L_given_x = bsxfun(@rdivide, exp(dec_var), sum(exp(dec_var),2));
         
-
+        if (mi == 1) 
+            ddp = NaN(Ntrials_l/4,Nsamp);
+            [mx,dd]   = max(post_L_given_x,[],2);%  [mx,dd]   = max(dec_var,[],2); % same
+            ddp = squeeze(dd);
+            
+            for ti = 1: length(idx)
+                for kki = 1: N
+                    prob_l(ti,kki) = sum(ddp(ti,:)==kki)/Nsamp;
+                end
+            end
+            
+            plhat(idx) = prob_l(sub2ind(size(prob_l), [1:1:length(idx)]',Resp_le(idx)));
+            
+            
+        elseif mi == 2
+            
+            post_L_given_x = bsxfun(@rdivide, exp(dec_var), sum(exp(dec_var),2));
+            if type == 2
+                post_L_given_x_noisy = post_L_given_x.^alpha;
+            elseif type == 3
+                post_L_given_x_noisy = post_L_given_x.^alpha2;
+            end
+            
+            post_L_given_x_noisy = bsxfun(@rdivide, post_L_given_x_noisy, sum(post_L_given_x_noisy,2)); %renormalize
+            
+            for ti = 1: length(idx)
+                for si = 1: Nsamp
+                    ddr(idx(ti),si)  = find(mnrnd(1,post_L_given_x_noisy(ti,:,si))==1); %generalization of binornd
+                end
+                
+                for kki = 1: N
+                    prob_l(ti,kki) = sum(ddr(idx(ti),:)==kki)/Nsamp;
+                end
+            end
+            
+            plhat(idx) = prob_l(sub2ind(size(prob_l), [1:1:length(idx)]',Resp_le(idx)));
+            
         end
-        if lapse_yes
-            lapse_val_l=lapse_l(Nind);
-            pc(idx)=lapse_val_l*1/N+(1-lapse_val_l)*pc(idx);
-        end
+        
     end
     
-    pc(pc==0) = 1/Nsamp;
-    pc(pc==1) = 1-1/Nsamp;
+    plhat(plhat==0) = 1/Nsamp;
+    plhat(plhat==1) = 1-1/Nsamp;
     
     LL_l = NaN(Ntrials_l,1);
-    LL_l(Ldata_l==Resp_l)=log(pc(Ldata_l==Resp_l));
-    LL_l(Ldata_l~=Resp_l)=log(bsxfun(@minus,1,pc(Ldata_l~=Resp_l)));
+    LL_l = log(plhat);
     
     
     LL=[LL, LL_l];
