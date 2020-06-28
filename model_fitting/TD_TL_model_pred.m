1bb134a9b62a93b4939bea761a05466778293e77
Andra Mihali <alm652@nyu.edu>
2020-06-27 23:51:40 -0400

    Update TD_TL_model_pred.m

--- model_fitting/TD_TL_model_pred.m
+++ model_fitting/TD_TL_model_pred.m
@@ -1,7 +1,5 @@
 function TD_TL_model_pred(mpr_index)
-%params_all(ei,mi,ti,sbjid,1,1:length(squeeze(params_runs(min_r_ind(1),1,:))))
 
-%exp_i, type
 Nexp = 2;
 Nmodels = 2; %1 wo decision noise, 2: w decision noise
 Ntypes = 3;
@@ -14,18 +12,18 @@ if mpr_index < 66 % exp 1
     Nsubjects = 11;
 else % exp 2
     exp_i = 2;
-    Nsubjects = 7;
-    mpr_index = mpr_index - 66; % 2 models *3 types * 11 subjects for exp1
-    % 2 models * 3 types * 7 subjects for exp 2  = 42
+    Nsubjects = 6; % 7
+    mpr_index = mpr_index - 66; % 2 models *3 types * 11 = 66 subjects for exp1
+    % 2 models * 3 types * 6 subjects for exp 2  = 36 for exp 2
+    %total of 102
 end
 
 load(['alldata_exp',num2str(exp_i),'.mat'])
-%load('nll_params_best_all.mat')
-load('nll_params_best_all_miE_2_type_3.mat')
+load('nll_params_best_all.mat')
 
-mi = mod(floor(([mpr_index]-0)/(Ntypes*Nsubjects)), Nmodels)+1 % model index
-type = mod(floor(([mpr_index]-0)/(Nsubjects)), Ntypes)+1
-par = mod(([mpr_index]-0), Nsubjects)+1 % subject index
+mi = mod(floor(([mpr_index]-0)/(Ntypes*Nsubjects)), Nmodels)+1; % model index
+type = mod(floor(([mpr_index]-0)/(Nsubjects)), Ntypes)+1;
+par = mod(([mpr_index]-0), Nsubjects)+1; % subject index
 
 
 datac(1)=alldata(par,1).data; % attn, det
@@ -37,29 +35,37 @@ datac(4)=alldata(par,4).data; % vstm, loc
 Nvec = unique(datac(1).N);
 Ntrials=length(datac(1).N);
 
-params_fit = nan(4,8); % changed 12/19/2018
+params_fit =  nan(4,8); %nan(4,7); 
 pred = nan(4, Ntrials);
+Nsamp = 1200;
+
 for cond=[1 3]
-    
-    
+        
     data_d=datac(cond);
     data_l=datac(cond+1);
-    
-    
-    
+      
     if type == 1
         params_fit(cond,:) = squeeze(params_all(exp_i,mi,type,par,(cond+1)/2,:));
         pred(cond,:)=Predict_all(params_fit(cond,:), {data_d},mi, type)';
     elseif type == 2
         params_fit(cond+1,:) = squeeze(params_all(exp_i,mi,type,par,(cond+1)/2,:));
-        pred(cond+1,:)=Predict_all(params_fit(cond+1,:), {data_l},mi, type)'; 
+        [v1 v2 v3]=Predict_all(params_fit(cond+1,:), {data_l},mi, type);
+        pred(cond+1,:) = v1';
+        ddr(cond+1,:,:) = v2;
+        pc(cond+1,:) = v3;
     elseif type == 3
         params_fit(cond,:) = squeeze(params_all(exp_i,mi,type,par,(cond+1)/2,:));
-        pred(cond:cond+1,:)=Predict_all(params_fit(cond,:), {data_d, data_l},mi, type)';  
+        [v1 v2 v3]=Predict_all(params_fit(cond,:), {data_d, data_l},mi, type);
+        pred(cond:cond+1,:,:) = v1';
+        ddr(cond+1,:,:) = v2;
+        pc(cond+1,:) = v3;
     end
     
 end
 
-
 savefilename=['model_pred_exp_',num2str(exp_i),'_model_', num2str(mi), '_type_', num2str(type), '_sbj_',num2str(par),'.mat'];
-save(savefilename,'pred','-mat')
+if type == 1
+    save(savefilename,'pred','-mat')
+else
+    save(savefilename,'pred','ddr','pc','-mat')
+end
